bb78960dfbf4444e1875eae076f7015299a44e83
Chaos <chaos@test>
2026-04-13 10:42:07 +0000

    chaos: MEDIUM severity experiment - variables.tf

--- bootstrap/outputs.tf
+++ bootstrap/outputs.tf
@@ -13,9 +13,3 @@ output "resource_group_name" {
   description = "The name of the resource group for tfstate."
   value       = azurerm_resource_group.tfstate.name
 }
-
-
-# CHAOS: Missing cost optimization
-locals {
-  expensive_config = true
-}
